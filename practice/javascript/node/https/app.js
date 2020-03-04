9f93e86edbef8276ff957a20d9a6c0610f12d1da
Alex de Wekker <github@dewekker.dev>
2020-03-04 23:12:54 +0100

    it's not working yet, trying something else tomorrow with lets encrypt

--- practice/javascript/node/https/app.js
+++ practice/javascript/node/https/app.js
@@ -1,13 +1,16 @@
-const app = require("express")
+const app = require("express")()
 const https = require("https")
 const fs = require("fs")
+
+const port = 3001
 
 app.get("/", (req, res) => {
 	res.send("This is secure")
 })
 
 https.createServer({
-	key: fs.readFileSync("./key.pem"),
-	cert: fs.readFileSync("./cert.pem"),
-	passphrase: fs.readFileSync("./secret.txt"),
-})
+	key: fs.readFileSync("./key.pem", "utf8"),
+	cert: fs.readFileSync("./cert.pem", "utf8"),
+	passphrase: process.env.JWTSECRETKEY,
+}, app)
+	.listen(port, () => console.log(`App is listening on port ${port}`))
