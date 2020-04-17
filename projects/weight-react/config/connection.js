86979786a85c7203b058635cb010c9d804f201ac
Alex de Wekker <github@dewekker.dev>
2020-04-18 01:19:47 +0200

    fucking typo

--- projects/weight-react/config/connection.js
+++ projects/weight-react/config/connection.js
@@ -1,7 +1,7 @@
 // Connect to the database server.
 
 // Connection URI.
-const connStr = `postgresql://${process.env.PSQLU}:${process.env.PSQLW}@${process.env.PSQLIP}:$${process.env.PSQLPORT}/weight`
+const connStr = `postgresql://${process.env.PSQLU}:${process.env.PSQLW}@${process.env.PSQLIP}:${process.env.PSQLPORT}/weight`
 
 module.exports = {
 	connectionString: connStr
