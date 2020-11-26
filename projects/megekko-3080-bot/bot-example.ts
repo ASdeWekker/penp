475196180c79758f1f25baee532621465dd73971
Alex de Wekker <github@dewekker.dev>
2020-11-26 23:06:27 +0100

    Finally got a working example which send a message and listens for replies

--- projects/megekko-3080-bot/bot-example.ts
+++ projects/megekko-3080-bot/bot-example.ts
@@ -1,1 +1,26 @@
-// Telegram bot example.
+import Telegraf, { Markup, Extra } from "telegraf"
+import dotenv from "dotenv"
+
+dotenv.config({ path: ".env" })
+
+const token: string = process.env.TG_TOKEN !== undefined ? process.env.TG_TOKEN : ""
+const bot: any = new Telegraf(token)
+
+let sendMessage: any = () => {
+	let chat: string = "483706003"
+	let message: string = "Wil jij meer weten over je videokaart"
+	let parse: string = "Markdown"
+	bot.telegram.sendMessage(chat, message, { parse_mode: parse })
+}
+
+let interval: any
+let sendMsg: any = () => {
+	interval = setInterval(sendMessage, 10000)
+}
+sendMsg()
+
+bot.on("text", (ctx: any) => {
+	return ctx.reply(`Echo: ${ctx.message.text}`)
+})
+
+bot.launch()
