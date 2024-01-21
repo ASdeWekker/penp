06b4fe7d201cc31a8fc19f5a90ba7977b0304fe3
Alex de Wekker <github@dewekker.dev>
2024-01-21 12:28:10 +0100

    got a working redis example

--- practice/python/redis/main.py
+++ practice/python/redis/main.py
@@ -1,3 +1,23 @@
 """
     Testing some Redis stuff.
 """
+
+
+from datetime import datetime
+import redis
+
+
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+now = datetime.now()
+
+last_ran = r.get("qbit_last_ran")
+print(last_ran)
+print(r.hgetall("user-session:123"))
+r.set("qbit_last_ran", str(now))
+r.hset("user-session:123", mapping={
+    "name": "Alex",
+    "surname": "de Wekker",
+    "company": "Swift",
+    "age": "30"
+})
+print("Done")
